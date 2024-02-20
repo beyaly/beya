676b2870681d1538b6f90fcee729a738fa30e5a9
beyaly <romerobeaallysa@gmail.com>
2024-02-20 12:33:39 +0800

    Update department_caption.py

--- day-01/TASKS/department_caption.py
+++ day-01/TASKS/department_caption.py
@@ -2,14 +2,13 @@
 """ 🚀Deploying...I'm <𝗡𝗮𝗺𝗲>, representing the <𝗗𝗲𝗽𝗮𝗿𝘁𝗺𝗲𝗻𝘁>. I'm thrilled to commence our expedition into the vast and uncharted realm of the <𝗚𝗮𝗹𝗮𝘅𝘆>. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻 """
 
 # Write the code ↓ to read user's input.
-
-
-
-
+name = input("Enter your name: ")
 
 # Write the code ↓ to display the department's caption.
+department = input("Enter your AWS Cloud Club Department: ")
+
 # Select and employ a string concatenation method based on your personal preference and comfort level.
+galaxy = input("Specify the galaxy to which you belong: ")
 
-
-
+print(f"🚀Deploying...I'm {name}, representing the {department}. I'm thrilled to commence our expedition into the vast and uncharted realm of the {galaxy}. Together, we'll navigate this exciting frontier of technology! 🌌🌟💻")
 
